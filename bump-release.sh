8c51882cadeaeb2d40f34652fad6a1459071cc1b
liamzebedee <liamzebedee@yahoo.com.au>
2019-06-17 15:30:28 +0200

    Add dirty tree check

--- bump-release.sh
+++ bump-release.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
-set -ex
+set -e
 
 if ! [ -x "$(command -v jq)" ]; then
   echo 'Error: jq is not installed.' >&2
+  exit 1
+fi
+
+if ! [[ -z $(git diff-index HEAD) ]]; then
+  echo 'Tree is dirty. Commit first'
   exit 1
 fi
 
